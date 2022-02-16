a077620b05be83d0da8cffb3bc7fb90b1eeb069d
exopyl <exopyl74@mail.ru>
2022-02-16 20:14:49 +0100

    tests for yahoo scraping

--- test/test_scraping_yahoo.py
+++ test/test_scraping_yahoo.py
@@ -7,7 +7,6 @@ from src import yahoo
 class TestScrapingYahoo:
 
     def check_expectations(self, df, csvfile):
-        return
         assert(isinstance(df, pd.DataFrame))
         df.to_csv("./test/generated/"+csvfile)
         df_generated = pd.read_csv("./test/generated/"+csvfile)
@@ -16,61 +15,61 @@ class TestScrapingYahoo:
 
     def test_get_list_nasdaq(self):
         df = yahoo.get_list_NASDAQ()
-        self.check_expectations(df, "list_nasdaq.csv")
+        self.check_expectations(df, "yahoo_list_nasdaq.csv")
 
     def test_get_list_yahoo_sp500(self):
         df = yahoo.get_list_yahoo_SP500()
-        self.check_expectations(df, "list_sp500.csv")
+        self.check_expectations(df, "yahoo_list_sp500.csv")
 
     def test_get_list_dow(self):
         df = yahoo.get_list_DOW()
-        self.check_expectations(df, "list_dow.csv")
+        self.check_expectations(df, "yahoo_list_dow.csv")
        
     def test_get_list_ftse100(self):
         df = yahoo.get_list_FTSE100()
-        self.check_expectations(df, "list_ftse100.csv")
+        self.check_expectations(df, "yahoo_list_ftse100.csv")
 
     def test_get_list_ftse250(self):
         df = yahoo.get_list_FTSE250()
-        self.check_expectations(df, "list_ftse250.csv")
+        self.check_expectations(df, "yahoo_list_ftse250.csv")
 
     def test_get_list_ibovespa(self):
         df = yahoo.get_list_IBOVESPA()
-        self.check_expectations(df, "list_ibovespa.csv")
+        self.check_expectations(df, "yahoo_list_ibovespa.csv")
 
     def test_get_list_nifty50(self):
         df = yahoo.get_list_NIFTY50()
-        #self.check_expectations(df, "list_nifty50.csv")
+        #self.check_expectations(df, "yahoo_list_nifty50.csv")
 
     def test_get_list_nifty_bank(self):
         df = yahoo.get_list_NIFTY_BANK()
-        self.check_expectations(df, "list_nifty_bank.csv")
+        self.check_expectations(df, "yahoo_list_nifty_bank.csv")
 
     def test_get_list_euronext(self):
         df = yahoo.get_list_EURONEXT()
-        self.check_expectations(df, "list_euronext.csv")
+        self.check_expectations(df, "yahoo_list_euronext.csv")
 
     def test_get_list_undervalued(self):
         pass
         #df = yahoo.get_list_undervalued()
-        #self.check_expectations(df, "list_undervalued.csv")
+        #self.check_expectations(df, "yahoo_list_undervalued.csv")
 
     def test_get_list_losers(self):
         pass
         #df = scrap_yahoo_list.get_list_losers()
-        #self.check_expectations(df, "list_losers.csv")
+        #self.check_expectations(df, "yahoo_list_losers.csv")
 
     def test_get_list_gainers(self):
         pass
         #df = yahoo.get_list_gainers()
-        #self.check_expectations(df, "list_gainers.csv")
+        #self.check_expectations(df, "yahoo_list_gainers.csv")
 
     def test_get_list_most_actives(self):
         pass
         #df = yahoo.get_list_most_actives()
-        #self.check_expectations(df, "list_most_actives.csv")
+        #self.check_expectations(df, "yahoo_list_most_actives.csv")
 
     def test_get_list_trending_tickers(self):
         pass
         #df = yahoo.get_list_trending_tickers()
-        #self.check_expectations(df, "list_trending_tickers.csv")
+        #self.check_expectations(df, "yahoo_list_trending_tickers.csv")
