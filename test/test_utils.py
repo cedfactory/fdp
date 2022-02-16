e7f653c4f51102926ec4593154b2cead90bed202
exopyl <exopyl74@mail.ru>
2022-02-16 22:03:04 +0100

    define a constant

--- test/test_utils.py
+++ test/test_utils.py
@@ -4,8 +4,9 @@ from pandas.testing import assert_frame_equal
 
 def check_expectations(df, csvfile):
     assert(isinstance(df, pd.DataFrame))
-    df.to_csv("./test/generated/"+csvfile)
-    df_generated = pd.read_csv("./test/generated/"+csvfile)
-    os.remove("./test/generated/"+csvfile)
+    generated_file = "./test/generated/"+csvfile
+    df.to_csv(generated_file)
+    df_generated = pd.read_csv(generated_file)
+    os.remove(generated_file)
     df_ref = pd.read_csv("./test/references/"+csvfile)
     assert_frame_equal(df_generated, df_ref)
