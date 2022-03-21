72d7cb13bf31f3253a601d241626840394ce7415
exopyl <88846154+exopyl@users.noreply.github.com>
2022-03-21 13:25:00 +0100

    fix

--- src/yf_wrapper.py
+++ src/yf_wrapper.py
@@ -1,5 +1,10 @@
 import time
 import yfinance as yf
+from inspect import getframeinfo, stack
+
+def print_exception_info(exception):
+    caller = getframeinfo(stack()[2][0])
+    print("[{}:{}] - {}".format(caller.filename, caller.lineno, exception))
 
 def get_info(value):
     if value == None:
@@ -14,25 +19,31 @@ def get_info(value):
         info["isin"] = yf_value.isin
     except BaseException as e:
         info["isin"] = '-'
+        print_exception_info(e)
     try:
         info["industry"] = yf_value.info['industry']
     except BaseException as e:
+        print_exception_info(e)
         pass
     try:
         info["sector"] = yf_value.info['sector']
     except BaseException as e:
+        print_exception_info(e)
         pass
     try:
         info["short_name"] = yf_value.info['shortName']
     except BaseException as e:
+        print_exception_info(e)
         pass
     try:
         info["country"] = yf_value.info['country']
     except BaseException as e:
+        print_exception_info(e)
         pass
     try:
         info["exchange"] = yf_value.info['exchange']
     except BaseException as e:
+        print_exception_info(e)
         pass    
 
     return {"status":"ok", "info":info}
