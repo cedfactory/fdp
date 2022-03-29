1e964f3f1c2cb4f67a3972e13ddf39cde33a76c6
exopyl <88846154+exopyl@users.noreply.github.com>
2022-03-29 17:50:02 +0200

    parameters for portfolio

--- src/portfolio.py
+++ src/portfolio.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from . import crypto, tradingview
 
-def get_portfolio():
+def get_portfolio(recommendations=["BUY", "STRONG_BUY"], intervals=["15m", "30m", "1h"]):
     df_gainers = crypto.get_top_gainers(50)
     symbols = df_gainers['symbol'].to_list()
-    df_recommendations = tradingview.filter_with_tradingview_recommendations(symbols, ["BUY", "STRONG_BUY"])
+    df_recommendations = tradingview.filter_with_tradingview_recommendations(symbols, recommendations, intervals)
     df_portfolio = pd.merge(df_gainers, df_recommendations)
+    print(df_portfolio)
     return df_portfolio
