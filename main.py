98be699a22ec1bc27d5d8e68475c6de6342eeb77
exopyl <88846154+exopyl@users.noreply.github.com>
2022-08-02 10:31:57 +0200

    change port 8080

--- main.py
+++ main.py
@@ -100,4 +100,4 @@ def get_portfolio():
     return response
 
 if __name__ == "__main__":
-    app.run(debug=False, host= '0.0.0.0', port=5000)
+    app.run(debug=False, host= '0.0.0.0', port=8080)
