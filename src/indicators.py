55303344dc7b2c4734820b7b114f49cdacd9e2c3
exopyl <88846154+exopyl@users.noreply.github.com>
2022-08-03 11:09:20 +0200

    cleaning

--- src/indicators.py
+++ src/indicators.py
@@ -5,86 +5,7 @@ from finta import TA
 import numpy as np
 from . import indicators_vsa as vsa
 from . import indicators_flabeling as flabeling
-
-class SuperTrend():
-    def __init__(
-        self,
-        high,
-        low,
-        close,
-        atr_window=10,
-        atr_multi=3
-    ):
-        self.high = high
-        self.low = low
-        self.close = close
-        self.atr_window = atr_window
-        self.atr_multi = atr_multi
-        self._run()
-        
-    def _run(self):
-        # calculate ATR
-        price_diffs = [self.high - self.low, 
-                    self.high - self.close.shift(), 
-                    self.close.shift() - self.low]
-        true_range = pd.concat(price_diffs, axis=1)
-        true_range = true_range.abs().max(axis=1)
-        # default ATR calculation in supertrend indicator
-        atr = true_range.ewm(alpha=1/self.atr_window,min_periods=self.atr_window).mean() 
-        # atr = ta.volatility.average_true_range(high, low, close, atr_period)
-        # df['atr'] = df['tr'].rolling(atr_period).mean()
-        
-        # HL2 is simply the average of high and low prices
-        hl2 = (self.high + self.low) / 2
-        # upperband and lowerband calculation
-        # notice that final bands are set to be equal to the respective bands
-        final_upperband = hl2 + (self.atr_multi * atr)
-        final_lowerband = hl2 - (self.atr_multi * atr)
-        
-        # initialize Supertrend column to True
-        supertrend = [True] * len(self.close)
-        
-        for i in range(1, len(self.close)):
-            curr, prev = i, i-1
-            
-            # if current close price crosses above upperband
-            if self.close[curr] > final_upperband[prev]:
-                supertrend[curr] = True
-            # if current close price crosses below lowerband
-            elif self.close[curr] < final_lowerband[prev]:
-                supertrend[curr] = False
-            # else, the trend continues
-            else:
-                supertrend[curr] = supertrend[prev]
-                
-                # adjustment to the final bands
-                if supertrend[curr] == True and final_lowerband[curr] < final_lowerband[prev]:
-                    final_lowerband[curr] = final_lowerband[prev]
-                if supertrend[curr] == False and final_upperband[curr] > final_upperband[prev]:
-                    final_upperband[curr] = final_upperband[prev]
-
-            # to remove bands according to the trend direction
-            if supertrend[curr] == True:
-                final_upperband[curr] = np.nan
-            else:
-                final_lowerband[curr] = np.nan
-                
-        self.st = pd.DataFrame({
-            'Supertrend': supertrend,
-            'Final Lowerband': final_lowerband,
-            'Final Upperband': final_upperband
-        })
-        
-    def super_trend_upper(self):
-        return self.st['Final Upperband']
-        
-    def super_trend_lower(self):
-        return self.st['Final Lowerband']
-        
-    def super_trend_direction(self):
-        return self.st['Supertrend']
-
-
+from . import indicators_supertrend as supertrend
 
 def compute_indicators(df, indicators, keep_only_requested_indicators = False, params = None):
     if not isinstance(df, pd.DataFrame):
@@ -186,7 +107,7 @@ def compute_indicators(df, indicators, keep_only_requested_indicators = False, p
             df['outcomes_vsa'] = df.close.pct_change(-1)
 
         elif indicator == "super_trend_direction":
-            super_trend = SuperTrend(
+            st = supertrend.SuperTrend(
                     df['high'], 
                     df['low'], 
                     df['close'], 
@@ -194,7 +115,7 @@ def compute_indicators(df, indicators, keep_only_requested_indicators = False, p
                     5 # self.st_short_atr_multiplier
                 )
                 
-            df['super_trend_direction'] = super_trend.super_trend_direction()
+            df['super_trend_direction'] = st.super_trend_direction()
             df['super_trend_direction'] = df['super_trend_direction'].shift(1)
 
 
