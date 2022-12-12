6bd5d8be85562b5273c7f2f7898e34e6ad15f99b
exopyl <exopyl74@mail.ru>
2022-12-12 11:31:32 +0100

    change version

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def add_headers(response):
 
 @app.route("/")
 def hello():
-	return '''Hello world ! (v1.1)'''
+	return '''Hello world ! (v1.2)'''
 
 @app.route('/list', methods=['OPTIONS', 'GET'])
 def get_list():
