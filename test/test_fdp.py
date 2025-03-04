9956ed11f84f2759f61da3129e87af0d709759a7
exopyl <exopyl74@mail.ru>
2025-03-04 22:54:30 +0100

    clean fdp_manager

--- test/test_fdp.py
+++ test/test_fdp.py
@@ -6,7 +6,7 @@ from src import fdp
 class TestFDP:
 
     def test_fdp_bitget(self):
-        my_fdp = fdp.FDP({"exchange_name": "bitget"})
+        my_fdp = fdp.FDPCCXT(params = {"type": "ccxt", "id": "ccxt1", "exchange": "bitget"})
 
         res = my_fdp.get_symbol_ohlcv_last("BTC")
 
