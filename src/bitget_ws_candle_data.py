6b52390be55f7caea2cc98384f3dd95815a1d0b6
Extracheesy <desportes.cedric@gmail.com>
2025-04-27 13:35:55 +0200

    ws error monitoring

--- src/bitget_ws_candle_data.py
+++ src/bitget_ws_candle_data.py
@@ -51,14 +51,16 @@ class WSCandleData:
 
         # 4) Bulk‐update existing rows
         for idx, row in to_update:
-            existing_df.loc[idx] = row
+            with self._lock:
+                existing_df.loc[idx] = row
             # print(f"+ {symbol_key} {timeframe} updated: {idx} at now (UTC): {datetime.datetime.now(datetime.timezone.utc)}")  # CEDE DEBUG
 
         # 5) Bulk‐append new rows (as a single concat)
         if to_append:
             append_df = pd.DataFrame(to_append, index=[r.name for r in to_append])
-            existing_df = pd.concat([existing_df, append_df], axis=0)
-            existing_df.sort_index(inplace=True)
+            with self._lock:
+                existing_df = pd.concat([existing_df, append_df], axis=0)
+                existing_df.sort_index(inplace=True)
             # for idx in append_df.index: # CEDE DEBUG
             #     print(f"- {symbol_key} {timeframe} added: {idx} at now (UTC): {datetime.datetime.now(datetime.timezone.utc)}") # CEDE DEBUG
 
@@ -89,8 +91,8 @@ class WSCandleData:
         if not symbol_key.endswith("USDT"):
             symbol_key += "USDT"
 
-        with self._lock:
-            if self.state[symbol_key].get(timeframe) is None:
-                # return pd.DataFrame(columns=["open", "high", "low", "close", "volume"])
-                return None
-            return self.state[symbol_key].get(timeframe).tail(length)
+        # with self._lock:
+        if self.state[symbol_key].get(timeframe) is None:
+            # return pd.DataFrame(columns=["open", "high", "low", "close", "volume"])
+            return None
+        return self.state[symbol_key].get(timeframe).tail(length)
